73e70f2028bbe86526de783e9f3fe055cc02e63b
gunkim <gunkim0318@gmail.com>
2020-12-24 17:39:27 +0900

    쿼리 실행 시 콘솔에 로그 찍도록 설정

--- src/test/java/com/market/app/db/ConnectionTest.java
+++ src/test/java/com/market/app/db/ConnectionTest.java
@@ -1,5 +1,8 @@
 package com.market.app.db;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,23 +10,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
+@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class ConnectionTest {
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/market?serverTimezone=UTC&useUnicode=true&characterEncoding=utf8&useSSL=false";
-    private static final String USER ="root";
-    private static final String PW = "";
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
 
     @Test
-    public void 커넥션_테스트() throws ClassNotFoundException {
-        Class.forName(DRIVER);
-
-        try(Connection con = DriverManager.getConnection(URL, USER, PW)) {
-            System.out.println("커넥션 성공!");
-        } catch (SQLException e) {
+    public void 커넥션_테스트(){
+        try(Connection con = sqlSessionFactory.openSession().getConnection()){
+            log.info("커넥션 성공 !");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void 마이바티스_테스트(){
+        try(SqlSession sqlSession = sqlSessionFactory.openSession()){
+            sqlSession.selectList("com.market.app.db.selectTest");
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
