5ee43b6d719becbf3aa814d87fdbf840ec849908
gunkim <gunkim0318@gmail.com>
2020-12-28 09:42:50 +0900

    상품 API 컨트롤러 작성

---
+++ src/main/java/com/market/app/web/ProductController.java
@@ -0,0 +1,10 @@
+package com.market.app.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 상품 API
+ */
+@RequestMapping("/api/products")
+public class ProductController {
+}
